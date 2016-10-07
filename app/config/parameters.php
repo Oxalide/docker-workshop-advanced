8810994fb4f6ceb3c3753748c1a104c35f771242
Théo Chamley <theo.ch@mley.fr>
2016-10-07 16:03:53 +0200

    Déroulé du workshop

--- app/config/parameters.php
+++
@@ -1,6 +0,0 @@
-<?php
-$container->setParameter('database.host', getenv('SYMFONY__DATABASE__HOST'));
-$container->setParameter('database.port', getenv('SYMFONY__DATABASE__PORT'));
-$container->setParameter('database.name', getenv('SYMFONY__DATABASE__NAME'));
-$container->setParameter('database.user', getenv('SYMFONY__DATABASE__USER'));
-$container->setParameter('database.password', getenv('SYMFONY__DATABASE__PASSWORD'));
